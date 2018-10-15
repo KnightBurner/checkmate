705c9befca0d4d9a7edfb298c36fcaf82dbed66e
Tristan Beasley <twbeasley13@gmail.com>
2018-10-15 01:31:52 -0400

    added chessboard with divs

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,19 @@
 class GamesController < ApplicationController
   def show
   end
+
+  def index
+    @games = Game.all
+  end
+
+  def new
+     @game = Game.new
+   end
+
+  def create 
+  end
+
+  def show
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
  root 'static_pages#index'
   get "/games/:game" => "games#show"
+ resources :games
 end
