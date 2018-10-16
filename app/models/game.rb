331e51bcb3c2774e0e1e6bdd954f6c7259f9d2e5
Tristan Beasley <twbeasley13@gmail.com>
2018-10-16 15:57:22 -0400

    added piece_at function

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ApplicationRecord
   scope :available, -> { where(black_player_id: nil).or(where(white_player_id: nil)) }
+  
+  def piece_at_(x, y)
+    chess_pieces.where(position_x: x, position_y: y).first
+  end
 end
