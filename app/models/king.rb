d3cc214ee15443ac9240ce774cdfe23feb2bc634
Jake Karl <jakekarl3@gmail.com>
2018-10-16 03:35:36 +0000

    valid_move? method for king

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,2 +1,16 @@
 class King < ChessPiece
+
+  def move_one_space?(start, stop)
+    (start - stop).abs = 1
+  end
+
+  def valid_move?(board, stop)
+    if self.is_vertical_move?(board, stop)
+      return move_one_space?(self.position_y, stop.position_y)
+    elsif self.is_horizontal_move?(board, stop)
+      return move_one_space?(self.position_x, stop.position_x)
+    else self.is_diagonal_move?(board, stop)
+      return move_one_space?(self.position_y, stop.position_y) && move_one_space?(self.position_x, stop.position_x)
+    end
+  end
 end
