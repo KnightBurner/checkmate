0cec284b98e0e23417242384b90f7347742401e5
Tristan Beasley <twbeasley13@gmail.com>
2018-10-22 17:46:26 -0400

    solved routing errors added missing files

---
+++ db/migrate/20181022172311_change_postion_to_position.rb
@@ -0,0 +1,7 @@
+class ChangePostionToPosition < ActiveRecord::Migration[5.0]
+  def change
+    rename_column :chess_pieces, :postion_x, :position_x
+    rename_column :chess_pieces, :postion_y, :position_y
+    
+  end
+end
