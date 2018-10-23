3f0ecb3bc9e2d0efbbc6c84f44a45394be64d90c
Jake Karl <jakekarl3@gmail.com>
2018-10-23 02:26:56 +0000

    Completed queen valid_move method

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,13 @@
 class Queen < ChessPiece
+
+  def valid_move?(board, stop)
+    if is_space_occupied?(board, stop)
+      if is_opponent(board, stop)
+        return !is_obstructed?(start, stop)
+      end
+    else
+      return !is_obstructed?(start, stop)
+    end
+  end
+
 end
