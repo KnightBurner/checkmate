f3e96f0bc5081bab7fc5523fcf4082d1e1fdb37d
Jake Karl <jakekarl3@gmail.com>
2018-10-23 02:35:02 +0000

    Completed bishop valid_move method

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -6,4 +6,16 @@ class Bishop < ChessPiece
       return "&#9815;"
     end
   end
+
+  def valid_move?(board, stop)
+    if is_diagonal_move?(stop)
+      if is_space_occupied?(board, stop)
+        if is_opponent(board, stop)
+          return !is_obstructed?(start, stop)
+        end
+      else
+        return !is_obstructed?(start, stop)
+      end
+    end
+  end
 end
