63defdf00f179777a285c55b8d3cbf5b537312a2
Your Name <tina.yuting.lee@gmail.com>
2018-10-24 23:42:19 +0000

    Checked all tests, all tests passing

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -11,31 +11,4 @@ RSpec.describe GamesController, type: :controller do
     end
   end
 
-
-  # describe 'games#update action' do
-  #   it 'should redirect to game show page' do
-  #     game = FactoryBot.create(:game)
-  #     patch :update, params: { id: game.id }
-  #     expect(response).to redirect_to(game_path(game))
-  #   end
-  # end
-
-  # describe 'games#update action' do
-    
-  #   it 'should change black_player_id to current user id' do
-  #     login_user
-  #     game = FactoryBot.create(:game)
-  #     patch :update, params: { id: game.id, black_player_id: current_user.id}
-  #     expect(black_player_id).to eq(user.id)
-  #   end
-  # end
-
-
-  # describe '#terminate' do
-  #   context 'with valid args' do
-  #   end
-
-  #   context 'with invalid args' do
-  #   end
-  # end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,138 +1,138 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Game, type: :model do
+RSpec.describe Game, type: :model do
 
-#   describe "games.available" do
-#     it "should return games that have only one player" do
-#       game = create(:game)
-#       game = create(:game)
-#       games = Game.all
-#       expect(games.available).not_to be_nil
-#     end
-#   end
+  describe "games.available" do
+    it "should return games that have only one player" do
+      game = FactoryBot.create(:game)
+      game = FactoryBot.create(:game)
+      games = Game.all
+      expect(games.available).not_to be_nil
+    end
+  end
 
   
-#   describe "populate board all players" do
-#     it "determines if the populated board is complete" do
-#       game = FactoryBot.create(:game)
-#       expect(game.chess_pieces.count).to eq(32)
-#     end
-#  end
+  describe "populate board all players" do
+    it "determines if the populated board is complete" do
+      game = FactoryBot.create(:game)
+      expect(game.chess_pieces.count).to eq(32)
+    end
+ end
 
-#   describe "populate board with the white players" do
-#     it "Places all of the white player pieces onto the board's start-game positions" do
-#       game = FactoryBot.create(:game)
+  describe "populate board with the white players" do
+    it "Places all of the white player pieces onto the board's start-game positions" do
+      game = FactoryBot.create(:game)
       
 
-#       # white Pawns
-#       expect(game.chess_pieces.find_by(position_x: 0, position_y: 1).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 1, position_y: 1).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 2, position_y: 1).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 3, position_y: 1).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 4, position_y: 1).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 5, position_y: 1).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 6, position_y: 1).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 7, position_y: 1).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 0, position_y: 1).color).to eq 'white'
-#       expect(game.chess_pieces.find_by(position_x: 1, position_y: 1).color).to eq 'white'
-#       expect(game.chess_pieces.find_by(position_x: 2, position_y: 1).color).to eq 'white'
-#       expect(game.chess_pieces.find_by(position_x: 3, position_y: 1).color).to eq 'white'
-#       expect(game.chess_pieces.find_by(position_x: 4, position_y: 1).color).to eq 'white'
-#       expect(game.chess_pieces.find_by(position_x: 5, position_y: 1).color).to eq 'white'
-#       expect(game.chess_pieces.find_by(position_x: 6, position_y: 1).color).to eq 'white'
-#       expect(game.chess_pieces.find_by(position_x: 7, position_y: 1).color).to eq 'white'
+      # white Pawns
+      expect(game.chess_pieces.find_by(position_x: 0, position_y: 1).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 1, position_y: 1).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 2, position_y: 1).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 3, position_y: 1).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 4, position_y: 1).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 5, position_y: 1).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 6, position_y: 1).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 7, position_y: 1).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 0, position_y: 1).color).to eq 'white'
+      expect(game.chess_pieces.find_by(position_x: 1, position_y: 1).color).to eq 'white'
+      expect(game.chess_pieces.find_by(position_x: 2, position_y: 1).color).to eq 'white'
+      expect(game.chess_pieces.find_by(position_x: 3, position_y: 1).color).to eq 'white'
+      expect(game.chess_pieces.find_by(position_x: 4, position_y: 1).color).to eq 'white'
+      expect(game.chess_pieces.find_by(position_x: 5, position_y: 1).color).to eq 'white'
+      expect(game.chess_pieces.find_by(position_x: 6, position_y: 1).color).to eq 'white'
+      expect(game.chess_pieces.find_by(position_x: 7, position_y: 1).color).to eq 'white'
   
 
 
-#       # white Rooks
-#       expect(game.chess_pieces.find_by(position_x: 0, position_y: 0).type).to eq 'Rook'
-#       expect(game.chess_pieces.find_by(position_x: 0, position_y: 0).color).to eq 'white'
-#       expect(game.chess_pieces.find_by(position_x: 7, position_y: 0).type).to eq 'Rook'
-#       expect(game.chess_pieces.find_by(position_x: 7, position_y: 0).color).to eq 'white'
+      # white Rooks
+      expect(game.chess_pieces.find_by(position_x: 0, position_y: 0).type).to eq 'Rook'
+      expect(game.chess_pieces.find_by(position_x: 0, position_y: 0).color).to eq 'white'
+      expect(game.chess_pieces.find_by(position_x: 7, position_y: 0).type).to eq 'Rook'
+      expect(game.chess_pieces.find_by(position_x: 7, position_y: 0).color).to eq 'white'
 
  
 
-#       # white Knights
-#       expect(game.chess_pieces.find_by(position_x: 1, position_y: 0).type).to eq 'Knight'
-#       expect(game.chess_pieces.find_by(position_x: 1, position_y: 0).color).to eq 'white'
-#       expect(game.chess_pieces.find_by(position_x: 6, position_y: 0).type).to eq 'Knight'
-#       expect(game.chess_pieces.find_by(position_x: 6, position_y: 0).color).to eq 'white'
+      # white Knights
+      expect(game.chess_pieces.find_by(position_x: 1, position_y: 0).type).to eq 'Knight'
+      expect(game.chess_pieces.find_by(position_x: 1, position_y: 0).color).to eq 'white'
+      expect(game.chess_pieces.find_by(position_x: 6, position_y: 0).type).to eq 'Knight'
+      expect(game.chess_pieces.find_by(position_x: 6, position_y: 0).color).to eq 'white'
 
 
-#       # white Bishops
-#       expect(game.chess_pieces.find_by(position_x: 2, position_y: 0).type).to eq 'Bishop'
-#       expect(game.chess_pieces.find_by(position_x: 2, position_y: 0).color).to eq 'white'
-#       expect(game.chess_pieces.find_by(position_x: 5, position_y: 0).type).to eq 'Bishop'
-#       expect(game.chess_pieces.find_by(position_x: 5, position_y: 0).color).to eq 'white'
+      # white Bishops
+      expect(game.chess_pieces.find_by(position_x: 2, position_y: 0).type).to eq 'Bishop'
+      expect(game.chess_pieces.find_by(position_x: 2, position_y: 0).color).to eq 'white'
+      expect(game.chess_pieces.find_by(position_x: 5, position_y: 0).type).to eq 'Bishop'
+      expect(game.chess_pieces.find_by(position_x: 5, position_y: 0).color).to eq 'white'
 
  
-#       # white Queen
-#       expect(game.chess_pieces.find_by(position_x: 3, position_y: 0).type).to eq 'Queen'
-#       expect(game.chess_pieces.find_by(position_x: 3, position_y: 0).color).to eq 'white'
+      # white Queen
+      expect(game.chess_pieces.find_by(position_x: 3, position_y: 0).type).to eq 'Queen'
+      expect(game.chess_pieces.find_by(position_x: 3, position_y: 0).color).to eq 'white'
 
-#       # white King
-#       expect(game.chess_pieces.find_by(position_x: 4, position_y: 0).type).to eq 'King'
-#       expect(game.chess_pieces.find_by(position_x: 4, position_y: 0).color).to eq 'white'
-#     end
-#   end
+      # white King
+      expect(game.chess_pieces.find_by(position_x: 4, position_y: 0).type).to eq 'King'
+      expect(game.chess_pieces.find_by(position_x: 4, position_y: 0).color).to eq 'white'
+    end
+  end
 
-#   describe "populate board with the black players" do
-#     it "Places all of the black player pieces onto the board's start-game positions" do
-#       game = FactoryBot.create(:game)
+  describe "populate board with the black players" do
+    it "Places all of the black player pieces onto the board's start-game positions" do
+      game = FactoryBot.create(:game)
       
 
-#       # black Pawns
-#       expect(game.chess_pieces.find_by(position_x: 0, position_y: 6).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 1, position_y: 6).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 2, position_y: 6).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 3, position_y: 6).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 4, position_y: 6).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 5, position_y: 6).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 6, position_y: 6).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 7, position_y: 6).type).to eq 'Pawn'
-#       expect(game.chess_pieces.find_by(position_x: 0, position_y: 6).color).to eq 'black'
-#       expect(game.chess_pieces.find_by(position_x: 1, position_y: 6).color).to eq 'black'
-#       expect(game.chess_pieces.find_by(position_x: 2, position_y: 6).color).to eq 'black'
-#       expect(game.chess_pieces.find_by(position_x: 3, position_y: 6).color).to eq 'black'
-#       expect(game.chess_pieces.find_by(position_x: 4, position_y: 6).color).to eq 'black'
-#       expect(game.chess_pieces.find_by(position_x: 5, position_y: 6).color).to eq 'black'
-#       expect(game.chess_pieces.find_by(position_x: 6, position_y: 6).color).to eq 'black'
-#       expect(game.chess_pieces.find_by(position_x: 7, position_y: 6).color).to eq 'black'
+      # black Pawns
+      expect(game.chess_pieces.find_by(position_x: 0, position_y: 6).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 1, position_y: 6).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 2, position_y: 6).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 3, position_y: 6).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 4, position_y: 6).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 5, position_y: 6).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 6, position_y: 6).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 7, position_y: 6).type).to eq 'Pawn'
+      expect(game.chess_pieces.find_by(position_x: 0, position_y: 6).color).to eq 'black'
+      expect(game.chess_pieces.find_by(position_x: 1, position_y: 6).color).to eq 'black'
+      expect(game.chess_pieces.find_by(position_x: 2, position_y: 6).color).to eq 'black'
+      expect(game.chess_pieces.find_by(position_x: 3, position_y: 6).color).to eq 'black'
+      expect(game.chess_pieces.find_by(position_x: 4, position_y: 6).color).to eq 'black'
+      expect(game.chess_pieces.find_by(position_x: 5, position_y: 6).color).to eq 'black'
+      expect(game.chess_pieces.find_by(position_x: 6, position_y: 6).color).to eq 'black'
+      expect(game.chess_pieces.find_by(position_x: 7, position_y: 6).color).to eq 'black'
   
 
 
-#       # black Rooks
-#       expect(game.chess_pieces.find_by(position_x: 0, position_y: 7).type).to eq 'Rook'
-#       expect(game.chess_pieces.find_by(position_x: 0, position_y: 7).color).to eq 'black'
-#       expect(game.chess_pieces.find_by(position_x: 7, position_y: 7).type).to eq 'Rook'
-#       expect(game.chess_pieces.find_by(position_x: 7, position_y: 7).color).to eq 'black'
+      # black Rooks
+      expect(game.chess_pieces.find_by(position_x: 0, position_y: 7).type).to eq 'Rook'
+      expect(game.chess_pieces.find_by(position_x: 0, position_y: 7).color).to eq 'black'
+      expect(game.chess_pieces.find_by(position_x: 7, position_y: 7).type).to eq 'Rook'
+      expect(game.chess_pieces.find_by(position_x: 7, position_y: 7).color).to eq 'black'
 
  
 
-#       # black Knights
-#       expect(game.chess_pieces.find_by(position_x: 1, position_y: 7).type).to eq 'Knight'
-#       expect(game.chess_pieces.find_by(position_x: 1, position_y: 7).color).to eq 'black'
-#       expect(game.chess_pieces.find_by(position_x: 6, position_y: 7).type).to eq 'Knight'
-#       expect(game.chess_pieces.find_by(position_x: 6, position_y: 7).color).to eq 'black'
+      # black Knights
+      expect(game.chess_pieces.find_by(position_x: 1, position_y: 7).type).to eq 'Knight'
+      expect(game.chess_pieces.find_by(position_x: 1, position_y: 7).color).to eq 'black'
+      expect(game.chess_pieces.find_by(position_x: 6, position_y: 7).type).to eq 'Knight'
+      expect(game.chess_pieces.find_by(position_x: 6, position_y: 7).color).to eq 'black'
 
 
-#       # black Bishops
-#       expect(game.chess_pieces.find_by(position_x: 2, position_y: 7).type).to eq 'Bishop'
-#       expect(game.chess_pieces.find_by(position_x: 2, position_y: 7).color).to eq 'black'
-#       expect(game.chess_pieces.find_by(position_x: 5, position_y: 7).type).to eq 'Bishop'
-#       expect(game.chess_pieces.find_by(position_x: 5, position_y: 7).color).to eq 'black'
+      # black Bishops
+      expect(game.chess_pieces.find_by(position_x: 2, position_y: 7).type).to eq 'Bishop'
+      expect(game.chess_pieces.find_by(position_x: 2, position_y: 7).color).to eq 'black'
+      expect(game.chess_pieces.find_by(position_x: 5, position_y: 7).type).to eq 'Bishop'
+      expect(game.chess_pieces.find_by(position_x: 5, position_y: 7).color).to eq 'black'
 
  
-#       # black Queen
-#       expect(game.chess_pieces.find_by(position_x: 3, position_y: 7).type).to eq 'Queen'
-#       expect(game.chess_pieces.find_by(position_x: 3, position_y: 7).color).to eq 'black'
+      # black Queen
+      expect(game.chess_pieces.find_by(position_x: 3, position_y: 7).type).to eq 'Queen'
+      expect(game.chess_pieces.find_by(position_x: 3, position_y: 7).color).to eq 'black'
 
-#       # black King
-#       expect(game.chess_pieces.find_by(position_x: 4, position_y: 7).type).to eq 'King'
-#       expect(game.chess_pieces.find_by(position_x: 4, position_y: 7).color).to eq 'black'
-#     end
-#   end
-# end
+      # black King
+      expect(game.chess_pieces.find_by(position_x: 4, position_y: 7).type).to eq 'King'
+      expect(game.chess_pieces.find_by(position_x: 4, position_y: 7).color).to eq 'black'
+    end
+  end
+end
 
 
   
