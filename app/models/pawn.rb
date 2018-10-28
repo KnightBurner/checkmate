8c675c8baeab4f6dfc902f5801b4f8e0d7bc49fd
lisarics <lisa.richardson@gmail.com>
2018-10-28 00:28:12 +0000

    fixed assignment operators

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -9,12 +9,12 @@ class Pawn < ChessPiece
   end
 
   def valid_move?(stop)
-    if is_first_move? && color = 'white'
-      return (position_y + 1 = stop[1]) || (position_y + 2 = stop[1])
-    elsif is_first_move? && color = 'black'
-      return (position_y - 1 = stop[1]) || (position_y - 2 = stop[1])
+    if is_first_move? && color == 'white'
+      return (position_y + 1 == stop[1]) || (position_y + 2 == stop[1])
+    elsif is_first_move? && color == 'black'
+      return (position_y - 1 == stop[1]) || (position_y - 2 == stop[1])
     elsif  !is_first_move? && !is_backwards_move?
-      return position_y + 1 = stop[1]
+      return position_y + 1 == stop[1]
     else is_diagonal_move(stop) && 
     # can only move diagonally if it's capturing an opponent
       
