f1fd619056f9b5db9cb8518b020dbe79adb42fc9
lisarics <lisa.richardson@gmail.com>
2018-10-28 00:23:15 +0000

    added is_first, is_backwards? and valid_move?

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,9 +1,23 @@
 class Pawn < ChessPiece
-  def display
-    if color == "black"
-      return "&#9823;"
-    else
-      return "&#9817;"
+
+  def is_first_move?
+    (color == 'white' && position_y == 1)  || (color == 'black' && position_y == 6)
+  end
+
+  def is_backwards_move?
+    if (color == 'white' && is_moving_up?(stop)) || (color == 'black' && is_moving_down?(stop))
+  end
+
+  def valid_move?(stop)
+    if is_first_move? && color = 'white'
+      return (position_y + 1 = stop[1]) || (position_y + 2 = stop[1])
+    elsif is_first_move? && color = 'black'
+      return (position_y - 1 = stop[1]) || (position_y - 2 = stop[1])
+    elsif  !is_first_move? && !is_backwards_move?
+      return position_y + 1 = stop[1]
+    else is_diagonal_move(stop) && 
+    # can only move diagonally if it's capturing an opponent
+      
     end
   end
 end
