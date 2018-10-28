31bb61771b38702c010dfcc356950ecb699fc787
Your Name <tina.yuting.lee@gmail.com>
2018-10-28 01:22:39 +0000

    cleared test database, failing test due to stored test data

--- spec/controllers/chess_pieces_controller_spec.rb
+++ spec/controllers/chess_pieces_controller_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ChessPiecesController, type: :controller do
   describe 'chess_pieces#show action' do
-    game = FactoryBot.create(:game)
+    
 
     it 'should successfully show page' do
+      game = FactoryBot.create(:game)
       get :show, params: { id: game.piece_at(0,0).id }
       expect(response).to have_http_status(:success)
     end
