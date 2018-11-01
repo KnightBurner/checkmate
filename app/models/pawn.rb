e87880e4246c3919378b4307602ee9b4074391ba
lisarics <lisa.richardson@gmail.com>
2018-11-01 17:34:57 +0000

    refactored the logic

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,11 +1,5 @@
 class Pawn < ChessPiece
 
-  def is_first_move?(board)
-    (is_white? && position_y == 1)  || (is_black? && position_y == 6)
-  end
-
-
-
   def is_white?
     color == 'white'
   end
@@ -14,24 +8,32 @@ class Pawn < ChessPiece
     color == 'black'
   end
 
+  def is_first_move_white?(board)
+    is_white? && position_y == 1
+  end
 
-  def valid_direction?(start, stop)
-    if is_white? && is_moving_down?(stop) || is_black && is_moving_up?(stop)
+  def is_first_move_black?(board)
+    is_black? && position_y == 6
   end
 
 
+  def white_valid_move_two?(board, stop)
+    is_first_move_white?(board) && (position_y + 2 == stop[1])
+  end
 
-  
+  def black_valid_move_two?(board, stop)
+    is_first_move_black?(board) && (position_y - 2 == stop[1])
+  end
+
+
+  def is_valid_direction?(start, stop)
+    (if is_white? && is_moving_down?(stop)) || (is_black && is_moving_up?(stop))
+  end
+
   def valid_move?(stop)
-    if is_first_move? && is_white? && is_moving_down?(stop)
-      return (position_y + 1 == stop[1]) || (position_y + 2 == stop[1])
-    elsif is_first_move? && is_black? && is_moving_up?(stop)
-      return (position_y - 1 == stop[1]) || (position_y - 2 == stop[1])
-    elsif  !is_first_move? && !is_backwards_move?
-      return position_y + 1 == stop[1]
-    else 
-      return is_diagonal_move?(stop) && stop != nil
-    # can only move diagonally if it's capturing an opponent     
+    if  (white_valid_move_two?(board, stop) || black_valid_move_two?(board, stop)) || (is_valid_direction?(start, stop) && spaces_between(start, stop) == 1)
+      || (is_diagonal_move?(stop) && stop != nil)
+      # can only move diagonally if it's capturing an opponent
     end
   end
 end
