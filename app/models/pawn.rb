08d7d9ce93e793829a864b923e4a86a48f26c4e4
lisarics <lisa.richardson@gmail.com>
2018-11-01 22:47:50 +0000

    refactored

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,23 +1,25 @@
 class Pawn < ChessPiece
 
- 
-
-  def valid_move_two?(stop)
-    (is_white? && position_y == 1 && stop[1] == 3) || 
-    (is_black? && position_y == 6 && stop[1] == 4)
+ def valid_move?(board, stop)
+    if  (valid_move_by_two?(stop) || (is_valid_direction?(start, stop) && spaces_between(start, stop) == 1 && !is_space_occupied(board, stop))
+      || (is_diagonal_move?(stop) && is_space_occupied?(board, stop) && is_opponent(board, stop))
+      # can only move diagonally if it's capturing an opponent
+    end
   end
 
+  private
+
+  def valid_move_by_two?(stop)
+    (is_white? && position_y == 1 && stop[1] == 3 && !is_space_occupied(board, stop)) || 
+    (is_black? && position_y == 6 && stop[1] == 4 && !is_space_occupied(board, stop))
+  end
 
   def is_valid_direction?(start, stop)
     (is_white? && is_moving_down?(stop)) || (is_black && is_moving_up?(stop))
   end
 
-  def valid_move?(stop)
-    if  (white_valid_move_two?(board, stop) || black_valid_move_two?(board, stop)) || (is_valid_direction?(start, stop) && spaces_between(start, stop) == 1)
-      || (is_diagonal_move?(stop) && stop != nil)
-      # can only move diagonally if it's capturing an opponent
-    end
-  end
+  def spaces_between(start, stop)
+    spaces = (start...stop).to_a.drop(1)
+    spaces.length
+  end 
 end
-
-
