395b9d34039eefe174fdc5097645a656fdfe1f9d
lisarics <lisa.richardson@gmail.com>
2018-11-01 19:36:16 +0000

    more work

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,33 +1,15 @@
 class Pawn < ChessPiece
 
-  def is_white?
-    color == 'white'
-  end
+ 
 
-  def is_black?
-    color == 'black'
-  end
-
-  def is_first_move_white?(board)
-    is_white? && position_y == 1
-  end
-
-  def is_first_move_black?(board)
-    is_black? && position_y == 6
-  end
-
-
-  def white_valid_move_two?(board, stop)
-    is_first_move_white?(board) && (position_y + 2 == stop[1])
-  end
-
-  def black_valid_move_two?(board, stop)
-    is_first_move_black?(board) && (position_y - 2 == stop[1])
+  def valid_move_two?(stop)
+    (is_white? && position_y == 1 && stop[1] == 3) || 
+    (is_black? && position_y == 6 && stop[1] == 4)
   end
 
 
   def is_valid_direction?(start, stop)
-    (if is_white? && is_moving_down?(stop)) || (is_black && is_moving_up?(stop))
+    (is_white? && is_moving_down?(stop)) || (is_black && is_moving_up?(stop))
   end
 
   def valid_move?(stop)
