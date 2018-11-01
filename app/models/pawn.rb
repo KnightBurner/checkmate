4c18ac4a55a70da9eb446bf10cce3606aabb110d
lisarics <lisa.richardson@gmail.com>
2018-11-01 23:06:09 +0000

    bug fix

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,15 +1,11 @@
 class Pawn < ChessPiece
 
  def valid_move?(board, stop)
-    if  (valid_move_by_two?(stop)) || (is_valid_direction?(start, stop) && spaces_between(start, stop) == 1 && !is_space_occupied(board, stop))
-      || (is_diagonal_move?(stop) && is_space_occupied?(board, stop) && is_opponent(board, stop))
-      # can only move diagonally if it's capturing an opponent
+    if  (valid_move_by_two?(stop)) || (is_valid_direction?(start, stop) && spaces_between(start, stop) == 1 && !is_space_occupied(board, stop)) || (is_diagonal_move?(stop) && is_space_occupied?(board, stop) && is_opponent(board, stop))
     end
   end
-
+ 
   private
-
-
 
   def valid_move_by_two?(stop)
     (is_white? && position_y == 1 && stop[1] == 3 && !is_space_occupied(board, stop)) || 
