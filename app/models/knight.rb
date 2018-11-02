529a49baf3e3c4fdbfe2369573036c23ce0daa1e
lisarics <lisa.richardson@gmail.com>
2018-11-02 20:14:25 +0000

    refactored

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -8,66 +8,19 @@ class Knight < ChessPiece
     end
   end
 
-  def valid_move?(board, stop)
-    if up_two_right?(board, stop) || up_two_left?(board, stop)  || down_two_right?(board, stop) || down_two_left(board, stop)
-      || up_one_right?(board, stop) || up_one_left?(board, stop) || down_one_right?(board, stop) || down_one_left?(board, stop)
-    end
+  def is_valid_move?(board, stop)
+    (is_horizontal_move?(board, stop) || is_vertical_move?(board, stop)) && (!chess_piece || chess_piece.color != color)
   end
 
   private
 
-  def is_obstructed?
-      nil
+  # moves 2 spaces horizontally and 1 space vertically
+  def is_horizontal_move?(board, stop)
+    (position_x - stop[0]).abs == 2 && (position_y - stop[1]).abs == 1
   end
 
-  def is_vertical?(board, stop)
-  end
-
-  def up_two_right?(board, stop)
-    # moves up 2 right 1
-      (is_moving_up(stop) && spaces_between(self.position_y, stop[1]) == 2)
-        && (is_moving_right(stop) && spaces_between (self.position_x, stop[0]) == 1)
-  end
-
-  def up_two_left?(board, stop)
-    # moves up 2 left 1
-    (is_moving_up(stop) && (spaces_between(self.position_y, stop[1]) == 2)
-      && (is_moving_left(stop) && spaces_between(self.position_x, stop[0]) == 1)
-  end
-
-  def down_two_right?(board, stop)
-    # moves down 2 right 1
-    (is_moving_down(stop) && (spaces_between(self.position_y, stop[1]) == 2)
-      && (is_moving_right(stop) && spaces_between(self.position_x, stop[0]) == 1)
-  end
-
-  def down_two_left?(board, stop)
-    # moves down 2 left 1
-    (is_moving_down(stop) && (spaces_between(self.position_y, stop[1]) == 2)
-      && (is_moving_left(stop) && spaces_between(self.position_x, stop[0]) == 1)
-  end
-
-  def up_one_right?(board, stop)
-    # moves up 1 right 2
-      (is_moving_up(stop) && spaces_between(self.position_y, stop[1]) == 1)
-        && (is_moving_right(stop) && spaces_between (self.position_x, stop[1]) == 2)
-  end
-
-  def up_one_left?(board, stop)
-    # moves up 1 left 2
-    (is_moving_up(stop) && (spaces_between(self.position_y, stop[1]) == 1)
-      && (is_moving_left(stop) && spaces_between(self.position_x, stop[0]) == 2)
-  end
-
-  def down_one_right?(board, stop)
-    # moves down 1 right 2
-    (is_moving_down(stop) && (spaces_between(self.position_y, stop[1]) == 1)
-      && (is_moving_right(stop) && spaces_between(self.position_x, stop[0]) == 2)
-  end
-
-  def down_one_left?(board, stop)
-    # moves down 1 left 2
-    (is_moving_down(stop) && (spaces_between(self.position_y, stop[1]) == 1)
-      && (is_moving_left(stop) && spaces_between(self.position_x, stop[0]) == 2)
-  end
+  # moves 2 spaces vertically and 1 space horizontally
+  def is_vertical_move?(board, stop)
+    (position_x - stop[0]).abs == 1 && (position_y - stop[1]).abs == 2
+  end  
 end
