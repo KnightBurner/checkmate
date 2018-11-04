f3678aa959f95034f18f953a35304fc848566894
twbeasley <39915785+twbeasley@users.noreply.github.com>
2018-11-04 09:35:31 -0700

    Update game.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
  root 'games#index'
- resources :games 
+ resources :games
  resources :chess_pieces
+ resources :chess_pieces, only:[:update]
 
 end
