67cca484cc0376def679dd72cf4310536d0644c7
Jake Karl <31318360+jakekarl3@users.noreply.github.com>
2018-11-05 21:48:51 -0600

    Update game_play_spec.rb

--- spec/features/game_play_spec.rb
+++ spec/features/game_play_spec.rb
@@ -1,19 +1,6 @@
 require 'rails_helper'
 include Warden::Test::Helpers
 
-feature "User game management", type: :feature do
-
-  scenario 'moving a piece' do
-    @game = FactoryBot.create(:game)
-    @chesspiece = @game.chess_pieces.first
-    visit "games/#{@game.id}"
-    link = find(:xpath, "//a[contains(@href, 'chess_pieces/#{@game.chess_pieces.first.id}')]")
-    link.click
-    expect(page).to have_content "Chesspiece Show page"
-  end
-
-end
-
 feature "Moving a piece", type: :feature do
 
 end
