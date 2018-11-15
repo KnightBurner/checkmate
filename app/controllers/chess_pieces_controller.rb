f71596233b581812199e23332cad7b9a95c6553f
lisarics <lisa.richardson@gmail.com>
2018-11-15 20:38:56 +0000

    hooked up move_to  & refactored the chess_pieces controller update action

--- app/controllers/chess_pieces_controller.rb
+++ app/controllers/chess_pieces_controller.rb
@@ -10,24 +10,17 @@ class ChessPiecesController < ApplicationController
     @game = @chesspiece.game
   end
 
+  # def update
+  #  @chesspiece = ChessPiece.find_by(id: params[:id])
+  #   @game = @chesspiece.game
+  #   @chesspiece.update_attributes(chesspiece_params)
+  #   render plain: 'updated!'
+  # end
+
   def update
-    @chesspiece = ChessPiece.find_by(id: params[:id])
-    @game = @chesspiece.game
-    @chesspiece.update_attributes(chesspiece_params)
-    render plain: 'updated!'
-  end
-
-  def edit
-    @chesspiece = ChessPiece.find_by(id: params[:id])
-  end
-
-   def move
     @chess_piece = Piece.find(params[:id])
-    @game = Game.find(params[:game_id])
-    position_x = params[:position_x]
-    position_y = params[:position_y]
     @chess_piece.move_to!([params[:position_x], params[:position_y]])
-    redirect_to game_path(params[:game_id])
+    redirect_to game_path(@chess_piece.game)
   end
 
   private
