f667879337e17a600248a631c0002c776fa220e6
lisarics <lisa.richardson@gmail.com>
2018-11-15 18:06:17 +0000

    working on chess pieces controller

--- app/controllers/chess_pieces_controller.rb
+++ app/controllers/chess_pieces_controller.rb
@@ -21,9 +21,20 @@ class ChessPiecesController < ApplicationController
     @chesspiece = ChessPiece.find_by(id: params[:id])
   end
 
+   def move
+    @chess_piece = Piece.find(params[:id])
+    @game = Game.find(params[:game_id])
+    position_x = params[:position_x]
+    position_y = params[:position_y]
+    @chess_piece.move_to!([params[:position_x], params[:position_y]])
+    redirect_to game_path(params[:game_id])
+  end
+
   private
 
   def chesspiece_params
     params.require(:chesspiece).permit(:position_x, :position_y)
   end
 end
+
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
  root 'games#index'
- resources :games 
- resources :chess_pieces
+ resources :chess_pieces, only:[:update]
 
 end
